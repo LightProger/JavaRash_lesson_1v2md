c2812d204099d63d95085809c197655b405ba80d
LightProger <photoluxe@mail.ru>
2016-12-25 00:30:45 +0600

    Урок 1 задача 3

---
+++ Elli/src/Vizitka.java
@@ -0,0 +1,14 @@
+/**
+ Напишите программу, которая в методе main объявляет такие переменные:
+ name типа String, age типа int и city типа String.
+ */
+public class Vizitka {
+    public static void main(String[] args) {
+        String name = "Никитченко Александр Николаевич";
+        int age = 38;
+        String city = "г. Норильск";
+        System.out.println("ФИО: " + name);
+        System.out.println("Возраст: " + age);
+        System.out.println("Место рождения: " + city);
+    }
+}
